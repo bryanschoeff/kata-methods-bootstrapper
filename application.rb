d553eae9796ffd358f1f3e1657654a0bbd096617
Bryan Schoeff <schoeff@gmail.com>
2013-01-21 13:24:42 -0500

    basic test skeleton and fake class skeleton for class-based katas

--- application.rb
+++ application.rb
@@ -3,12 +3,13 @@ require_relative 'models/KataBuilder.rb'
 require_relative 'models/KataClassmethod.rb'
 
 KataClass = "Array"
+OutputPath = "output/"
 
 methods = eval("#{KataClass}.methods")
 kata = Kata.new(KataClass, methods.map {|item| KataClassMethod.new(item)})
+
 tests = KataBuilder.get_tests(kata)
+fake = KataBuilder.get_fake(kata)
 
-
-print tests
-
-
+File.open("#{OutputPath}tests.rb", 'w') {|f| f.write(tests) }
+File.open("#{OutputPath}fake_#{kata.class_name.downcase}.rb", 'w') {|f| f.write(fake) }
--- models/KataBuilder.rb
+++ models/KataBuilder.rb
@@ -3,7 +3,13 @@ require 'ERB'
 class KataBuilder
   
   def self.get_tests(kata)
-    template_string = File.open('templates/Tests.rb.erb') { |f| f.read }
+    template_string = File.open('templates/tests.rb.erb') { |f| f.read }
+    template = ERB.new(template_string)
+    template.result(kata.get_binding)
+  end
+
+  def self.get_fake(kata)
+    template_string = File.open('templates/class.rb.erb') { |f| f.read }
     template = ERB.new(template_string)
     template.result(kata.get_binding)
   end
