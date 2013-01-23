7a225fc5e7f1635485c1e726967f7c008ff8553c
Bryan Schoeff <schoeff@gmail.com>
2013-01-22 22:55:38 -0500

    filtered out object methods and corrected the list of what we were after

---
+++ output/tests.rb
@@ -0,0 +1,445 @@
+require "minitest/autorun"
+
+describe "Array" do
+  before do
+    @list = Array.new
+  end
+
+  
+  it "supports &" do
+    # implement test for &  
+  end
+  
+  it "supports *" do
+    # implement test for *  
+  end
+  
+  it "supports +" do
+    # implement test for +  
+  end
+  
+  it "supports -" do
+    # implement test for -  
+  end
+  
+  it "supports <<" do
+    # implement test for <<  
+  end
+  
+  it "supports []=" do
+    # implement test for []=  
+  end
+  
+  it "supports all?" do
+    # implement test for all?  
+  end
+  
+  it "supports any?" do
+    # implement test for any?  
+  end
+  
+  it "supports assoc" do
+    # implement test for assoc  
+  end
+  
+  it "supports at" do
+    # implement test for at  
+  end
+  
+  it "supports chunk" do
+    # implement test for chunk  
+  end
+  
+  it "supports clear" do
+    # implement test for clear  
+  end
+  
+  it "supports collect" do
+    # implement test for collect  
+  end
+  
+  it "supports collect!" do
+    # implement test for collect!  
+  end
+  
+  it "supports collect_concat" do
+    # implement test for collect_concat  
+  end
+  
+  it "supports combination" do
+    # implement test for combination  
+  end
+  
+  it "supports compact" do
+    # implement test for compact  
+  end
+  
+  it "supports compact!" do
+    # implement test for compact!  
+  end
+  
+  it "supports concat" do
+    # implement test for concat  
+  end
+  
+  it "supports count" do
+    # implement test for count  
+  end
+  
+  it "supports cycle" do
+    # implement test for cycle  
+  end
+  
+  it "supports delete" do
+    # implement test for delete  
+  end
+  
+  it "supports delete_at" do
+    # implement test for delete_at  
+  end
+  
+  it "supports delete_if" do
+    # implement test for delete_if  
+  end
+  
+  it "supports detect" do
+    # implement test for detect  
+  end
+  
+  it "supports drop" do
+    # implement test for drop  
+  end
+  
+  it "supports drop_while" do
+    # implement test for drop_while  
+  end
+  
+  it "supports each" do
+    # implement test for each  
+  end
+  
+  it "supports each_cons" do
+    # implement test for each_cons  
+  end
+  
+  it "supports each_entry" do
+    # implement test for each_entry  
+  end
+  
+  it "supports each_index" do
+    # implement test for each_index  
+  end
+  
+  it "supports each_slice" do
+    # implement test for each_slice  
+  end
+  
+  it "supports each_with_index" do
+    # implement test for each_with_index  
+  end
+  
+  it "supports each_with_object" do
+    # implement test for each_with_object  
+  end
+  
+  it "supports empty?" do
+    # implement test for empty?  
+  end
+  
+  it "supports entries" do
+    # implement test for entries  
+  end
+  
+  it "supports fetch" do
+    # implement test for fetch  
+  end
+  
+  it "supports fill" do
+    # implement test for fill  
+  end
+  
+  it "supports find" do
+    # implement test for find  
+  end
+  
+  it "supports find_all" do
+    # implement test for find_all  
+  end
+  
+  it "supports find_index" do
+    # implement test for find_index  
+  end
+  
+  it "supports first" do
+    # implement test for first  
+  end
+  
+  it "supports flat_map" do
+    # implement test for flat_map  
+  end
+  
+  it "supports flatten" do
+    # implement test for flatten  
+  end
+  
+  it "supports flatten!" do
+    # implement test for flatten!  
+  end
+  
+  it "supports grep" do
+    # implement test for grep  
+  end
+  
+  it "supports group_by" do
+    # implement test for group_by  
+  end
+  
+  it "supports index" do
+    # implement test for index  
+  end
+  
+  it "supports inject" do
+    # implement test for inject  
+  end
+  
+  it "supports insert" do
+    # implement test for insert  
+  end
+  
+  it "supports join" do
+    # implement test for join  
+  end
+  
+  it "supports keep_if" do
+    # implement test for keep_if  
+  end
+  
+  it "supports last" do
+    # implement test for last  
+  end
+  
+  it "supports length" do
+    # implement test for length  
+  end
+  
+  it "supports map" do
+    # implement test for map  
+  end
+  
+  it "supports map!" do
+    # implement test for map!  
+  end
+  
+  it "supports max" do
+    # implement test for max  
+  end
+  
+  it "supports max_by" do
+    # implement test for max_by  
+  end
+  
+  it "supports member?" do
+    # implement test for member?  
+  end
+  
+  it "supports min" do
+    # implement test for min  
+  end
+  
+  it "supports min_by" do
+    # implement test for min_by  
+  end
+  
+  it "supports minmax" do
+    # implement test for minmax  
+  end
+  
+  it "supports minmax_by" do
+    # implement test for minmax_by  
+  end
+  
+  it "supports none?" do
+    # implement test for none?  
+  end
+  
+  it "supports one?" do
+    # implement test for one?  
+  end
+  
+  it "supports pack" do
+    # implement test for pack  
+  end
+  
+  it "supports partition" do
+    # implement test for partition  
+  end
+  
+  it "supports permutation" do
+    # implement test for permutation  
+  end
+  
+  it "supports pop" do
+    # implement test for pop  
+  end
+  
+  it "supports product" do
+    # implement test for product  
+  end
+  
+  it "supports push" do
+    # implement test for push  
+  end
+  
+  it "supports rassoc" do
+    # implement test for rassoc  
+  end
+  
+  it "supports reduce" do
+    # implement test for reduce  
+  end
+  
+  it "supports reject" do
+    # implement test for reject  
+  end
+  
+  it "supports reject!" do
+    # implement test for reject!  
+  end
+  
+  it "supports repeated_combination" do
+    # implement test for repeated_combination  
+  end
+  
+  it "supports repeated_permutation" do
+    # implement test for repeated_permutation  
+  end
+  
+  it "supports replace" do
+    # implement test for replace  
+  end
+  
+  it "supports reverse" do
+    # implement test for reverse  
+  end
+  
+  it "supports reverse!" do
+    # implement test for reverse!  
+  end
+  
+  it "supports reverse_each" do
+    # implement test for reverse_each  
+  end
+  
+  it "supports rindex" do
+    # implement test for rindex  
+  end
+  
+  it "supports rotate" do
+    # implement test for rotate  
+  end
+  
+  it "supports rotate!" do
+    # implement test for rotate!  
+  end
+  
+  it "supports sample" do
+    # implement test for sample  
+  end
+  
+  it "supports select" do
+    # implement test for select  
+  end
+  
+  it "supports select!" do
+    # implement test for select!  
+  end
+  
+  it "supports shift" do
+    # implement test for shift  
+  end
+  
+  it "supports shuffle" do
+    # implement test for shuffle  
+  end
+  
+  it "supports shuffle!" do
+    # implement test for shuffle!  
+  end
+  
+  it "supports size" do
+    # implement test for size  
+  end
+  
+  it "supports slice" do
+    # implement test for slice  
+  end
+  
+  it "supports slice!" do
+    # implement test for slice!  
+  end
+  
+  it "supports slice_before" do
+    # implement test for slice_before  
+  end
+  
+  it "supports sort" do
+    # implement test for sort  
+  end
+  
+  it "supports sort!" do
+    # implement test for sort!  
+  end
+  
+  it "supports sort_by" do
+    # implement test for sort_by  
+  end
+  
+  it "supports sort_by!" do
+    # implement test for sort_by!  
+  end
+  
+  it "supports take" do
+    # implement test for take  
+  end
+  
+  it "supports take_while" do
+    # implement test for take_while  
+  end
+  
+  it "supports to_a" do
+    # implement test for to_a  
+  end
+  
+  it "supports to_ary" do
+    # implement test for to_ary  
+  end
+  
+  it "supports transpose" do
+    # implement test for transpose  
+  end
+  
+  it "supports uniq" do
+    # implement test for uniq  
+  end
+  
+  it "supports uniq!" do
+    # implement test for uniq!  
+  end
+  
+  it "supports unshift" do
+    # implement test for unshift  
+  end
+  
+  it "supports values_at" do
+    # implement test for values_at  
+  end
+  
+  it "supports zip" do
+    # implement test for zip  
+  end
+  
+  it "supports |" do
+    # implement test for |  
+  end
+  
+end
